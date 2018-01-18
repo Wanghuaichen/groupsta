f5a45471ed3c02a2dfe5fb108cf6c5f1ffd96616
JopRijks <31651172+JopRijks@users.noreply.github.com>
2018-01-18 12:06:07 +0100

    User.py

--- application.py
+++ application.py
@@ -1,2 +1,12 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
+
+
 app = Flask(__name__)
+
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
---
+++ user.py
@@ -0,0 +1,3 @@
+def login(name, password):
+
+    
