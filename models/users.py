b90c3d665130898aca04db6b3f3b5d82ae45d7bd
Zjoerdie <schoffel07@gmail.com>
2018-01-19 20:13:03 +0000

    overgezet naar groups.py

--- models/users.py
+++ models/users.py
@@ -46,24 +46,3 @@ class User():
 
         else:
             return user[0]
-
-class Group():
-
-    def __init__(self, user_id, title, description):
-        self.user_id = user_id
-        self.title = title
-        self.description = description
-
-
-    def create(self):
-        # check if title already exist
-        result = db.execute("SELECT * FROM groups WHERE group_name = :title", title=self.title)
-        if len(result) != 0:
-            return None
-
-        # insert into database
-        group = db.execute("INSERT INTO groups (group_name, bio) VALUES (:group_name, :bio)", group_name = self.title, bio = self.description)
-
-        # select group id from the database
-        group_id = db.execute("SELECT group_id FROM groups WHERE group_name=:title", title=self.title)
-        return group_id[0]
