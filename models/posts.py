dfa0ba5dd849735f4da3dd8af9e523a1e0c89dcb
Zjoerdie <schoffel07@gmail.com>
2018-01-22 11:29:28 +0000

    posts is gekoppeld aan keuze van follow

--- models/posts.py
+++ models/posts.py
@@ -8,15 +8,16 @@ class Post():
         def __init__(self, user_id):
                 self.user_id = user_id
 
-        def upload(self, img_path):
+        def upload(self, img_path, group):
                 # retrieve username of user
                 name = db.execute("SELECT username FROM users WHERE user_id = :user_id", user_id = self.user_id)
 
                 # import post in database of user
-                db.execute("INSERT INTO posts (user_id, post_path, username) VALUES (:user_id, :post_path, :username)", \
+                db.execute("INSERT INTO posts (user_id, post_path, username, group_id) VALUES (:user_id, :post_path, :username, :group_id)", \
                             user_id = self.user_id, \
                             post_path = img_path, \
-                            username = name[0]["username"])
+                            username = name[0]["username"], \
+                            group_id = group)
 
         def loadgroups(self):
                 result = db.execute("SELECT * FROM follow where user_id = :user_id", user_id = self.user_id)
