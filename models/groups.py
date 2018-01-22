7d19cf1e46183d0c6a1d1404e9466057f0fa7a9a
Jop Rijksbarom <joprijksbaron@hotmail.nl>
2018-01-22 10:52:15 +0000

    groups.py follow werkt

--- models/groups.py
+++ models/groups.py
@@ -29,31 +29,26 @@ class Group():
         return group_id
 
     def follow(self):
-        # loads every group_id
-        group_ids = db.execute("SELECT group_id FROM groups")
-        group_ids = int(group_id[0]['group_id'])
 
-        # checks which group_id is requested
-        for i in group_ids:
-            if self.group_id == group_ids:
+        # collects groupname for the group_id
+        groupname = db.execute("SELECT group_name FROM groups WHERE group_id=:group_id", group_id=self.group_id)
+        groupname = str(groupname[0]['group_name'])
+        group_id = self.group_id
 
-                # collects groupname for the group_id
-                groupname = db.execute("SELECT groupname FROM groups WHERE group_id=:group_id", group_id=self.group_id)
+        # insert into new following request in the follow database
+        result = db.execute("INSERT INTO follow (user_id, group_id, groupname, admin) VALUES (:user_id, :group_id, :groupname, :admin)", \
+        user_id = self.user_id, group_id = group_id, groupname = groupname, admin = int(0))
 
-                # insert into new following request in the follow database
-                result = de.execute("INSERT INTO follow (user_id, group_id, groupname, admin) VALUES (:user_id, :group_id, :groupname, :admin)", \
-                user_id = self.user_id, group_id = group_id, groupname = groupname, admin = 0)
+        # returns follow_id to application.py
+        follow_id = db.execute("SELECT follow_id from follow WHERE group_id = :group_id AND user_id = :user_id", group_id = self.group_id, user_id = self.user_id)
+        follow_id = int(follow_id[0]['follow_id'])
 
-                # returns follow_id to application.py
-                follow_id = db.execute("SELECT follow_id from follow WHERE group_id = :group_id AND user_id = :user_id", group_id = self.group_id, user_id = self.user_id)
-                follow_id = int(follow_id[0]['follow_id'])
-                return follow_id
-        # if group doesn't exist return nothing
-        return None
+        return follow_id
 
     def loadgroups(self):
         # loads every group's name, description and every other kind of information about the groups
         result = db.execute("SELECT * FROM groups")
         return result
+
 
 
