649655462eaa5d350cd014c13e776c1378ce49da
Zjoerdie <schoffel07@gmail.com>
2018-01-23 11:09:59 +0000

    posts

--- models/posts.py
+++ models/posts.py
@@ -8,16 +8,18 @@ class Post():
         def __init__(self, user_id):
                 self.user_id = user_id
 
-        def upload(self, img_path, group):
+        def upload(self, img_path, group, description):
                 # retrieve username of user
                 name = db.execute("SELECT username FROM users WHERE user_id = :user_id", user_id = self.user_id)
 
                 # import post in database of user
-                db.execute("INSERT INTO posts (user_id, post_path, username, group_id) VALUES (:user_id, :post_path, :username, :group_id)", \
+                db.execute("INSERT INTO posts (user_id, post_path, username, group_id, description) \
+                        VALUES (:user_id, :post_path, :username, :group_id, :description)", \
                             user_id = self.user_id, \
                             post_path = img_path, \
                             username = name[0]["username"], \
-                            group_id = group)
+                            group_id = group, \
+                            description = description)
 
         def loadgroups(self):
                 result = db.execute("SELECT * FROM follow where user_id = :user_id", user_id = self.user_id)
