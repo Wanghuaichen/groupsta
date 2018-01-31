173fb13108c52e9e3290b457bdf1c4ab1bce1a01
sjorsie22 <31690155+sjorsie22@users.noreply.github.com>
2018-01-31 19:27:10 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,6 @@
 from flask import redirect, render_template, request, session
 import csv
 import urllib.request
-
 from flask import redirect, render_template, request, session
 from functools import wraps
 
